9f25fb0122b2267cb29fbf595eff35ba2eb7d234
DavidBarta92 <blj.david@gmail.com>
2020-07-15 14:13:05 +0200

    initial commit

---
+++ src/todo/Help.java
@@ -0,0 +1,45 @@
+package todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Help {
+
+    public static void allArguments() {
+        System.out.println("$ todo\n" +
+                "\n" +
+                "Command Line todo.Todo application\n" +
+                "=============================\n" +
+                "\n" +
+                "Command line arguments:\n" +
+                "    -l   Lists all the tasks\n" +
+                "    -a   Adds a new task\n" +
+                "    -r   Removes an task\n" +
+                "    -c   Completes an task");
+    }
+
+    public static void noTask() {
+        System.out.println("Unable to add: no task provided.");
+    }
+
+    public static void wrongNumber(String wrongNum) {
+        try {
+            List<Character> notNumber = new ArrayList<>();
+            for (char ch : wrongNum.toCharArray()) {
+                notNumber.add(ch);
+            }
+            if (notNumber.get(0) > 57 && notNumber.get(0) < 48) {
+                System.out.println("Unable to remove: index is not a number");
+            } else {
+                System.out.println("Unable to remove: index is out of bound");
+            }
+        } catch (Exception e) {
+            System.out.println("Unable to remove: no index provided");
+        }
+    }
+
+    public static void unsupportedArg() {
+        System.out.println("Unsupported argument");
+        Help.allArguments();
+    }
+}
---
+++ src/todo/Todo.java
@@ -0,0 +1,17 @@
+package todo;
+
+import java.io.IOException;
+
+public class Todo {
+    public static void main(String[] args) {
+        try {
+            if (args.length == 0) Help.allArguments();
+            else if (args[0].equals("-l")) Argument.list();
+            else if (args[0].equals("-a")) Argument.add(args[1]);
+            else if (args[0].equals("-r")) Argument.remove(args[1]);
+            else if (args[0].equals("-c")) Argument.check(args[1]);
+        } catch (Exception e) {
+            Help.unsupportedArg();
+        }
+    }
+}
